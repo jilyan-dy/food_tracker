9adef5e413057e28edf840bdff6d026e87a15932
jilyan-dy <jilyandy@gmail.com>
2022-11-25 23:03:35 +0800

    fixed homepage frontend

--- temp/src/pages/App.tsx
+++ temp/src/pages/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
+import './app.scss';
 
 import Layout from "../components/Layout"
 
 function App() {
   return (
     <Layout>
-      Hello
+      <div className="home">
+        <div className="wrapper">
+          <h1>Welcome ShiDa Fam!</h1>
+          <div className="actions">
+            <a href="/login">
+              <h3>Login</h3>
+            </a>
+            <span></span>
+            <a href="/register">
+              <h3>Register</h3>
+            </a>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
